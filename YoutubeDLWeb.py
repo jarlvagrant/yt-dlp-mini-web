9d8c3165ce45884ecc03c54b3056012eb7ec3c7e
jarlvagrant <53534207+jarlvagrant@users.noreply.github.com>
2025-03-03 17:05:12 -0800

    Update YoutubeDLWeb.py
    Update port number

--- YoutubeDLWeb.py
+++ YoutubeDLWeb.py
@@ -117,7 +117,7 @@ def main():
 	app.add_url_rule("/update_dir", view_func=UpdateDir.as_view("update_dir"))
 	app.add_url_rule("/youtube", view_func=YoutubeDownloader.as_view("youtube"))
 	app.add_url_rule("/progress_data/<uuid>", view_func=ProgressData.as_view("progress_data"))
-	app.run(debug=True)
+	app.run(debug=True, host='0.0.0.0', port=8008)
 
 
 if __name__ == "__main__":
