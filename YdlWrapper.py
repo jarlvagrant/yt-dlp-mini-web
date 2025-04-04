76d7e0166b40737fb4d50c6ac80fe96709aa5c18
jarlvagrant <jarlvagrant@gmail.com>
2025-04-04 16:29:01 -0700

    add support for playlist

--- YdlWrapper.py
+++ YdlWrapper.py
@@ -29,72 +29,100 @@ class UpdateDir(View):
 		return Response(response=response, status=200)
 
 
-class YoutubeDownloader(View):
-	outputTypes = {'video': 'mp4/bestvideo/best', 'audio': 'm4a/bestaudio/best'}
+output_formats = {'video': "bestvideo[ext=mp4][height<=720][vcodec!~=av01]+bestaudio[ext=m4a]/best[height<=720][ext=mp4]",
+                  'audio': 'bestaudio[ext=m4a][acodec!~=opus]/best'}
 
+
+class YoutubeDownloader(View):
 	def dispatch_request(self) -> ft.ResponseReturnValue:
 		return render_template("ydl.html", video_dir=ConfigIO.get("video_dir"), audio_dir=ConfigIO.get("audio_dir"))
 
 
 class Downloader:
-	def __init__(self, uuid, url, format):
+	def __init__(self, uuid, url, ext, output_dir, start = 0, end = 0):
 		self.uuid = uuid
-		self.url = url
-		self.format = format
+		self.urls = [url]
+		self.ext = ext
+		self.output_dir = output_dir
+		self.start = min(start, end) -1
+		self.end = max(start, end)
 		self.cur = 0
 		self.error = None
 		self.title = ""
+		self.completed = 0
+		self.status = "downloading"
 
-	def setTitle(self):
-		with yt_dlp.YoutubeDL({}) as ydl:
-			info_dict = ydl.extract_info(self.url, download=False)
+	def set_info(self):
+		with yt_dlp.YoutubeDL({'extract_flat' : "in_playlist"}) as ydl:
+			info_dict = ydl.extract_info(self.urls[0], download=False)
 			self.title = info_dict.get('title', '')
+			# playlist_count = info_dict.get('playlist_count', 0)
+			entries = info_dict.get("entries", None)
+			if entries:
+				self.urls = [entry.get("url", None) for entry in entries]
+				if self.start < 0 or self.start >= len(self.urls):
+					self.start = 0
+				if self.end <= 0 or self.end > len(self.urls):
+					self.end = len(self.urls)
+				self.urls = self.urls[self.start:self.end]
 			ydl.close()
 
+	# need work!
 	def my_hook(self, d):
-		try:
-			self.cur = str(d["_percent_str"]).strip().replace("%", "")
-			print("\nCurrent progress %s percent." % self.cur)
-		except:
-			print("\nUnknown progress percentage...")
+		self.status = d['status']
 		if d['status'] == 'finished':
-			self.cur = 100
+			self.status = "postprocessing"
+		self.cur = round(d["_percent"], 1)
+		print("\nCurrent progress %s percent." % self.cur)
 
 	def download_video(self):
-		if self.format == "true":
-			ext = 'mp4/bestvideo/best'
-			output_dir = ConfigIO.get("video_dir"),
-		else:
-			ext = 'm4a/bestaudio/best'
-			output_dir = ConfigIO.get("audio_dir"),
-		print("requested format %s; output directory %s." % (ext, output_dir))
+		print("requested format %s; output directory %s." % (self.ext, self.output_dir))
 		ydl_opts = {
-			"outtmpl": output_dir + '/%(title)s.%(ext)s',
-			# this is where you can edit how you'd like the filenames to be formatted
+			"outtmpl": self.output_dir + "/%(title)s.%(ext)s",
 			'progress_hooks': [self.my_hook],
-			'format': ext,
+			'format': self.ext,
 		}
 		with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-			self.error = ydl.download(self.url)
+			for url in self.urls:
+				if url:
+					self.title = f"{self.title} ({self.completed + 1}/{len(self.urls)})"
+					self.error = ydl.download(url)
+					self.completed += 1
+			self.status = "completed"
 			ydl.close()
 
 
 class ProgressData(View):
 	def dispatch_request(self, uuid) -> ft.ResponseReturnValue:
+		# verify output directory
+		resolution = request.args.get("resolution", "720", type=str)
+		if request.args.get("format") == "true":
+			ext = output_formats.get("video", "").replace("720", resolution)
+			output_dir = ConfigIO.get("video_dir")
+		else:
+			ext = output_formats.get("audio", "")
+			output_dir = ConfigIO.get("audio_dir")
+		if not os.path.isdir(output_dir):
+			data = jsonify(label="Initialization: ", cur="0", error=f"{output_dir} is not a valid directory!")
+			return data
+
 		global started_progress
 		if started_progress.get(uuid):
 			downloader = started_progress[uuid]
 		else:
 			print("create new downloader " + uuid)
-			downloader = Downloader(uuid, request.args.get("url"), request.args.get("format"))
+			downloader = Downloader(uuid, request.args.get("url"), ext, output_dir,
+			                        request.args.get("start", 0, type=int), request.args.get("end", 0, type=int))
 			started_progress[uuid] = downloader
 			try:
-				downloader.setTitle()
+				downloader.set_info()
 				downloader.download_video()
 			except Exception as ex:
 				downloader.error = ex.__str__()
 		if downloader.error or downloader.cur == 100:
 			started_progress.pop(uuid, None)
-		data = jsonify(label=downloader.title, cur=downloader.cur, error=downloader.error)
+		data = jsonify(label=downloader.title, status=downloader.status, cur=downloader.cur, error=downloader.error)
 		print(data.get_json())
 		return data
+
+
